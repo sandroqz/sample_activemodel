fc147e35d118b748b7b89a42710a3cc83500675e
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-02-03 14:52:13 -0300

    Creating new users from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!([{name: 'user1', email: "user1@domain.com", password: 'password1', password_confirmation: "password1"}, {name: "user2", email: "user2@domain.com", password: "password2", password_confirmation: "password2"}])
