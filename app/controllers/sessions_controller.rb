11fb17300b5de95586286d18107cb0baff993e2b
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-02-03 15:27:30 -0300

    Creating new and create methods in sessions controlller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+    @session = Session.new
+  end
+
+  def create
+    @session = Session.new params[:session]
+    if @session.valid?
+      @user = User.find_by_name(@session.name).try(:authenticate, @session.password)
+      if @user
+        redirect_to users_path, notice: "Welcome back"
+      else
+        redirect_to new_sessions_path, alert: "Login failed"
+      end
+    else
+      render action: "new"
+    end
+  end
 end
